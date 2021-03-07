231bbbf84ce027bf035d9015e88bc3cf52a876a1
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2021-03-07 16:23:35 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,3 @@
+import { API_URL, API_KEY } from "@env";
+
+export default { API_URL, API_KEY };
---
+++ index.js
@@ -0,0 +1,3 @@
+import { registerRootComponent } from "expo";
+import App from "./src/App.js";
+registerRootComponent(App);
