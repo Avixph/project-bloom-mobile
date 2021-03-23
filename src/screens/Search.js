6f2f7afaae76942f231285203fac2ff0bf78f6f4
david espinal <despinal0425@gmail.com>
2021-03-22 21:59:34 -0400

    added dispatch and use selector to the Search component to render list on screen

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,12 +1,23 @@
 import React from "react";
-// import { useState, useEffect } from "react";
+import { useState} from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+//Thunk
+import { fetchSearch } from "../redux/searchJobSlice"
+
 import {
   StyleSheet,
   SafeAreaView,
   Button,
   Text,
   TextInput,
+  TouchableOpacity,
+  View,
+  FlatList,
+  Image,
 } from "react-native";
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -20,9 +31,31 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
   },
+  companyLogo: {
+    width: 100,
+    height: 100,
+  }
 });
 
 export default function SearchScreen() {
+  const [search, setSearch] = useState("");
+
+  
+  const dispatch = useDispatch();
+  const jobsRequest = useSelector((state) => state.searches.searchJobs);
+  const status = useSelector((state) => state.searches.status);
+
+  console.log(jobsRequest.company_name);
+  
+  function handleSearch() {
+    const field = search;
+    dispatch(fetchSearch(field));
+  }
+
+
+  
+
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Home</Text>
@@ -30,8 +63,41 @@ export default function SearchScreen() {
         style={styles.input}
         placeholder="search for jobs"
         keyboardType="default"
+        onChangeText={((val) => setSearch(val))}
       />
-      <Button title="Search" />
+      <TouchableOpacity
+        onPress={ handleSearch }
+      >
+        <Text>Search</Text>
+      </TouchableOpacity>
+      <View>
+        <FlatList
+          data={jobsRequest}
+          renderItem={({ item }) => (
+            <View>
+              <Image
+                style={styles.companyLogo}
+                source={{uri: `${item.company_logo_url}`}}
+              />
+              <Text>{item.title}</Text>
+              <Text>{item.company_name}</Text>
+            </View>
+          )}
+        />
+
+
+        {/* {jobsRequest.map((item, index) => {
+          return (
+            <View key={index}>
+            <Image
+                source={{uri: `${item.company_logo_url}`}}
+              />
+              <Text>{item.title}</Text>
+              <Text>{item.company_name}</Text>
+            </View>
+          )
+        })} */}
+      </View>
     </SafeAreaView>
   );
 }
